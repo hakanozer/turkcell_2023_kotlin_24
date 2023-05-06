577043ce82399b9cfa3c8996fc87bc363a082453
Hakan ÖZER <hakanozer02@gmail.com>
2023-05-06 18:08:05 +0300

    Days-1

--- Days-1/src/main/kotlin/Main.kt
+++ Days-1/src/main/kotlin/Main.kt
@@ -1,11 +1,29 @@
 fun main(args: Array<String>) {
 
     // One Line Comment
-    
+
     /*
     * MultineLine
     * Comment
     * */
 
+    // Variable
+    // String -> karakter katarı
+    val name = "Mehmet"
+    val surname = "Bilmem"
+    var city = "Adıyaman"
+    city = "Hatay"
+    val date = "1981"
 
+    // Int -> Tam sayı türü
+    val age = 25
+
+    // Double -> Ondalıklı Değer Türü
+    val num = 40.7
+
+    // Boolean -> True, False Değer Türü
+    val status = false
+
+    val nameSurname = "$name $surname"
+    println(nameSurname)
 }
